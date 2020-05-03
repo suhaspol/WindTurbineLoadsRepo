bc20ded24e5cbb2095a2968e05fd8a75e920d6a0
suhaspol <suhaspol@yahoo.com>
2020-05-03 14:40:38 -0500

    Intialising new project file

---
+++ WindTurbineLoadsRepo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project will explore wind turbine loads using experimental field data and simulattion data.',
+    author='Suhas Pol',
+    license='',
+)
